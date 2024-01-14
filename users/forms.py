a303ca23c3b5994f89e39e0423b5defd26327403
AleksandrStrulkov <astrulkov@yandex.ru>
2024-01-14 03:28:16 +0300

    Improving templates

--- users/forms.py
+++ users/forms.py
@@ -40,5 +40,5 @@ class RegisterForm(StyleFormMixin, UserCreationForm):
 
 	class Meta:
 		model = User
-		fields = ('first_name', 'email', 'password1', 'password2', 'avatar', 'phone', 'is_staff')
+		fields = ('first_name', 'last_name', 'email', 'password1', 'password2', 'avatar', 'phone', 'is_staff')
 
